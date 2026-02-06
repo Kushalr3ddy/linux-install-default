c2b39815fd9127c0b9f07ca3a36cd7af1fb74a67
kushalr3ddy <46622551+Kushalr3ddy@users.noreply.github.com>
2026-02-06 13:13:53 +0530

    Add gnupg and ca-certificates to installation command

--- default.sh
+++ default.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # run this as root
-apt install neovim tmux terminator screen net-tools curl nmap wireguard htop git openssh-server openvpn unzip -y
+apt install neovim tmux terminator screen net-tools curl nmap wireguard htop git openssh-server openvpn unzip gnupg ca-certificates -y
 curl -fsSL https://tailscale.com/install.sh | sh
